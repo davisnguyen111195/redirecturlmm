023fb4dedc6e74a37a6a2de8d6ae7ca05829e6fb
realdatnguyen <nguyendat111195@gmail.com>
2018-10-16 22:45:00 +0700

    add project

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+
+const app = express()
+const port = process.env.port || 8080;
+
+app.use('/assets', express.static(__dirname + "/public"))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(morgan('dev'))
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/go', (req, res, next) => {
+    let link = 'https://123link.co/st?api=671277a48e917f3de5c2b1cd4bec64e89fb6844f&url=' + req.query.url
+    res.redirect(link)
+    next()
+})
+
+app.listen(port, () => {
+    console.log('App listening on port ' + port)
+})
